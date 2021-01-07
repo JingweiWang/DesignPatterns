807f5af29d9d7bb3e7f82e0e32ecfdb671a5f889
JingweiWang <13622032005@qq.com>
2021-01-07 23:14:05 +0800

    Singleton

--- src/io/github/jingweiwang/DesignPatterns/singleton/Client.java
+++ src/io/github/jingweiwang/DesignPatterns/singleton/Client.java
@@ -2,6 +2,9 @@ package io.github.jingweiwang.DesignPatterns.singleton;
 
 import io.github.jingweiwang.DesignPatterns.singleton.factory.FactorySingleton;
 
+import java.io.*;
+import java.lang.reflect.Constructor;
+
 public class Client {
     public static void main(String[] args) {
         SlackerSingleton.getInstance().doSomething();
@@ -10,5 +13,51 @@ public class Client {
         DoubleCheckLockSingleton.getInstance().doSomething();
         FactorySingleton.getInstance().doSomething();
         StaticInnerClzSingleton.getInstance().doSomething();
+
+        try {
+            reflectAttack();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            serializationAttack();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 反射攻击
+     *
+     * @throws Exception
+     */
+    private static void reflectAttack() throws Exception {
+        DoubleCheckLockSingleton singleton1 = DoubleCheckLockSingleton.getInstance();
+
+        Constructor<DoubleCheckLockSingleton> constructor = DoubleCheckLockSingleton.class.getDeclaredConstructor();
+        constructor.setAccessible(true);
+        DoubleCheckLockSingleton singleton2 = constructor.newInstance();
+
+        System.out.println(singleton1 == singleton2);
+    }
+
+    /**
+     * 反序列化攻击
+     *
+     * @throws Exception
+     */
+    private static void serializationAttack() throws Exception {
+        DoubleCheckLockSingleton singleton1 = DoubleCheckLockSingleton.getInstance();
+
+        ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream("serializationAttack"));
+        os.writeObject(singleton1);
+        os.close();
+
+        ObjectInputStream is = new ObjectInputStream(new FileInputStream("serializationAttack"));
+        DoubleCheckLockSingleton singleton2 = (DoubleCheckLockSingleton) is.readObject();
+        is.close();
+
+        System.out.println(singleton1 == singleton2);
     }
 }
--- src/io/github/jingweiwang/DesignPatterns/singleton/DoubleCheckLockSingleton.java
+++ src/io/github/jingweiwang/DesignPatterns/singleton/DoubleCheckLockSingleton.java
@@ -1,12 +1,29 @@
 package io.github.jingweiwang.DesignPatterns.singleton;
 
+import java.io.ObjectStreamException;
+import java.io.Serializable;
+
 /**
  * 双重检查锁定
  */
-public class DoubleCheckLockSingleton {
+public class DoubleCheckLockSingleton implements Serializable {
     private static volatile DoubleCheckLockSingleton instance = null;
 
+    private static volatile boolean initFlag = false;
+
+    // 防止反射攻击
     private DoubleCheckLockSingleton() {
+        if (!initFlag) {
+            synchronized (DoubleCheckLockSingleton.class) {
+                if (!initFlag) {
+                    initFlag = true;
+                } else {
+                    throw new RuntimeException("禁止非法调用构造器");
+                }
+            }
+        } else {
+            throw new RuntimeException("禁止非法调用构造器");
+        }
     }
 
     public static DoubleCheckLockSingleton getInstance() {
@@ -22,5 +39,10 @@ public class DoubleCheckLockSingleton {
 
     public void doSomething() {
         System.out.println("DoubleCheckLockingSingleton doSomething()");
+    }
+
+    // 防止反序列化攻击
+    private Object readResolve() throws ObjectStreamException {
+        return instance;
     }
 }
